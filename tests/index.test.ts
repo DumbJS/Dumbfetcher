c65e958b3e0dd56684696dabffacc06a60d427fe
Hugo Martin <hugomrt.94@gmail.com>
2023-06-30 20:53:34 +0200

    implement jest

---
+++ tests/index.test.ts
@@ -0,0 +1,7 @@
+import { DumbfetcherHooks, Dumbfetcher } from '../src/index';
+
+describe('testing index file', () => {
+    test('empty string should result in zero', () => {
+        console.log(DumbfetcherHooks);
+    });
+});
